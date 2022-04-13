9b863e838bf158e0ac0e965c0df0224c9903e12b
Sergio Siccha <siccha@mathematik.uni-kl.de>
2022-04-13 12:05:45 +0200

    tmux

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -o verbose # echo all commands
+
 # neovim things
 if (command -v nvim &>/dev/null) then
     echo "nvim installed"
 fi
-# TODO: If nvim isn't installed, make sure things like gitconfig don't
-# use nvim instead of vim
+
+# Nvim config
+git clone git@github.com:ssiccha/.vim $HOME/.vim
+
+# Symlinks to dotfiles
+./create_links
